531492fb179f71f44b07f318fef571d438d028bc
Danylo Lytovchenko <danylo.lytovchenko@gmail.com>
2018-02-21 23:57:14 +0200

    Final regex commit, search working

--- server/main.py
+++ server/main.py
@@ -4,6 +4,7 @@ import aiohttp_jinja2
 import motor.motor_asyncio
 import pymongo
 import json
+import re
 
 
 async def index_handler(request):
@@ -15,14 +16,14 @@ async def index_handler(request):
 
 async def autocomplete_handler(request):
     res = await app.collection.create_index([("word", pymongo.TEXT)])
-    # cursor = app.collection.find({"$text": {"$search": {'$regex': '/' + request.query.get('q') + '$/'}}}).sort("popularity").limit(3)
-    cursor = app.collection.find({"word": {'$regex': '/' + request.query.get('q') + '/', "$options": "$i"}}).sort("popularity").limit(3)
-    print('/' + request.query.get('q') + '$/', flush=True)
+    regex = re.compile(request.query.get('q'))
+    cursor = app.collection.find({"word": {"$regex": regex}})
+    print('/' + request.query.get('q') + '/', flush=True)
     print(cursor, flush=True)
     print(request.query.get('q'), flush=True)
     list_of_suggestions = []
-    for doc in await cursor.to_list(length=3):
-        print(doc.get('word'), flush=True)
+    for doc in await cursor.to_list(length=100):
+        print(doc, flush=True)
         list_of_suggestions.append({"word": doc.get('word')})
 
     return web.json_response(json.dumps(list_of_suggestions))
@@ -37,10 +38,3 @@ app.router.add_get('/auto_complete', autocomplete_handler)
 app.router.add_get('/', index_handler)
 
 web.run_app(app, host='0.0.0.0', port=80)
-
-    # cursor = app.collection.find({word: {'$search': request.query.get('q') }}).sort("popularity").limit(3)
-    # cursor = await app.db.command({"text": {"search": "dogs"}})
-    # print(dir(response), flush=True)
-    # print(response.result(), flush=True)
-    # for document in await cursor.to_list(length=3):
-    #     print(document, flush=True)
